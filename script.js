be63de6d6b86c852a98918d4abd9626ee06bd4d9
Sadullah TANRIKULU <sadullahtanrikulu@gmail.com>
2022-06-29 08:07:25 +0300

    can't get input value at first try

--- script.js
+++ script.js
@@ -6,21 +6,27 @@ const pauseBtn = document.querySelector('.pause-btn');
 
 const resetBtn = document.querySelector('.reset-btn');
 
-let startingMinutes = document.querySelector('#countdown-input').value = 1;
+let startingMinutes = document.querySelector('#countdown-input').value;
 console.log(startingMinutes);
 
 let time = startingMinutes * 60;
 console.log(time);
+
 let minutes = 0;
 
 let seconds = 0;
+
 const countdownEl = document.getElementById('countdown');
+
 countdownEl.innerHTML = `0${minutes} : 0${seconds}`;
 
 
 let startCountdown;
 
+
+
 function updateCountdown() {
+    
     minutes = Math.floor(time / 60);
     seconds = time % 60;
 
@@ -40,7 +46,7 @@ function updateCountdown() {
 }
 
 startBtn.addEventListener('click', () => {
-
+    startingMinutes = document.querySelector('#countdown-input').value;
     startCountdown = setInterval(updateCountdown, 1000);
 });
 
@@ -50,9 +56,13 @@ pauseBtn.addEventListener('click', () => {
 
 resetBtn.addEventListener('click', () => {
     clearInterval(startCountdown);
-    time = startingMinutes * 60
+    time = startingMinutes * 60;
     minutes = 0;
     seconds = 0;
     countdownEl.innerHTML = `${minutes} : 0${seconds}`;
 })
+
+window.onload = () => {
+    document.querySelector('#countdown-input').focus();
+}
 
